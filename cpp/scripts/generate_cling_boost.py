e64cef82f7f9088c5f20b76b1d0c489426025abc
Aditya Ardiya <aditya.ardiya@rapyuta-robotics.com>
2021-07-09 21:58:29 +0900

    Breaking changes: use load_boost.h instead of load_boost_git.h since it's not from git anymore

--- cpp/scripts/generate_cling_boost.py
+++ cpp/scripts/generate_cling_boost.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """
 Scripts that generate required `#pragma cling` for loading the boost library in the C++ Jupyter Notebook
-using the git source code
+using the conda env
 
 Usage:
-generate_cling_boost.py /path/to/boost
+generate_cling_boost.py
 """
 import argparse
 from pathlib import Path
@@ -19,18 +19,19 @@ def main():
     boost_conda_path = Path(os.environ['CONDA_PREFIX'] + "/include")
     path = boost_conda_path / "boost"
     if not path.exists():
-        print("[ERROR] Failed to find path", path, ". Are you sure you put the correct boost path?")
+        print("[ERROR] Failed to find boost. Please install via conda install -c conda-forge boost")
         from sys import exit
         exit(1)
 
     include_dirs = path.glob("**/include")
-    with open("load_boost_git.h", "w") as f:
+    filename = "load_boost.h"
+    with open(filename, "w") as f:
         f.write("#pragma once\n\n")
         for inc_dir in include_dirs:
             f.write('#pragma cling add_include_path("%s")\n' % str(inc_dir))
 
     os.makedirs(args.target_dir, exist_ok=True)
-    print("Files written to", "%s/load_boost_git.h"%args.target_dir)
+    print("Files written to", "%s/%s"%(args.target_dir, filename))
 
 
 if __name__ == "__main__":
