213349ac260e5a7acc51dd37be9b5bb68670d3ca
Aditya Ardiya <aditya.ardiya@rapyuta-robotics.com>
2021-07-09 12:29:58 +0900

    Formatting update

--- cpp/notebooks/jupyter_ros_utils/publisher.hpp
+++ cpp/notebooks/jupyter_ros_utils/publisher.hpp
@@ -1,3 +1,14 @@
+#pragma once
+
+#include <atomic>
+#include <sstream>
+#include <thread>
+
+#include <boost/function.hpp>
+
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+
 #include "xwidgets/xbutton.hpp"
 #include "xwidgets/xbox.hpp"
 #include "xwidgets/xtextarea.hpp"
@@ -6,93 +17,78 @@
 #include "xwidgets/xoutput.hpp"
 #include "xwidgets/xlabel.hpp"
 #include "xwidgets/xtext.hpp"
-#include <boost/function.hpp>
-#include <sstream>
-#include <ros/ros.h>
-#include "std_msgs/String.h"
-#include <ros/spinner.h>
 
-#include <cstring>
-#include <iostream>
-#include <string>
-#include <thread>
+#include "init.hpp"
 
-void ros_init(){
-    int argc = 1;
-    char *argv[1];
-    argv[0] = new char[9];
-    strcpy(argv[0], "jupyter_ros_pub");
-    ros::init(argc, argv, "jupyter_ros_pub");
-    static ros::AsyncSpinner spinner(4);
-    spinner.start();
-}
-
+/**
+ * @brief ROS Subscriber with widgets for jupyter notebook
+ */
 class ZeusPublisher {
-    private:
-        ros::Publisher pub;
-        xw::label pub_value;
-        xw::text  pub_value_txt;
-        xw::button btn;
+private:
+    ros::Publisher pub;
+    xw::label pub_value;
+    xw::text pub_value_txt;
+    xw::button btn;
 
-        xw::hbox main_layout;
-        xw::vbox left_section;
-        xw::vbox right_section;
-        xw::hbox pub_layout;
-        std_msgs::String value_pub;
-        bool stop_publisher = 1;
-        bool thr_running = false;
-        std::thread thr;
+    xw::hbox main_layout;
+    xw::vbox left_section;
+    xw::vbox right_section;
+    xw::hbox pub_layout;
+    std_msgs::String value_pub;
+    std::atomic<bool> thr_running = false;
+    std::thread thr;
+    size_t sleep_usec;
 
+public:
+    ZeusPublisher(const std::string& topic, int pub_queue_size = 10, size_t sleep_usec = 100'000)
+            : sleep_usec(sleep_usec) {
+        ros::NodeHandle nh;
+        pub = nh.advertise<std_msgs::String>(topic, pub_queue_size);
+        pub_value.value = "Input String to Publisher";
+        pub_value_txt.on_submit((std::bind(&ZeusPublisher::publish_msg, this)));
+        btn.description = "Publish Value";
+        btn.on_click([this]() {
+            if (thr_running) {
+                stop_thread();
+            } else {
+                start_thread();
+            }
+        });
 
-    public:
-        ZeusPublisher(std::string topic) {
-            ros::NodeHandle nh;
-            pub = nh.advertise<std_msgs::String>(topic, 10);
-            pub_value.value = "Input String to Publisher";
-            pub_value_txt.on_submit((std::bind(&ZeusPublisher::publish_msg, this)));
-            pub_layout.add(pub_value);
-            pub_layout.add(pub_value_txt);
-            left_section.add(pub_layout);
-            btn.description = "Publish Value";
-            btn.on_click([this](){
-                if (thr_running) {
-                    stop_thread();
-                } else {
-                    start_thread();
-                }
-            });
-            right_section.add(btn);
-            // layout
-            // pub_value | pub_value_text       btn
-            main_layout.add(left_section);
-            main_layout.add(right_section);
-            main_layout.display();
-        }
+        // setup layout
+        // pub_value | pub_value_text       btn
+        pub_layout.add(pub_value);
+        pub_layout.add(pub_value_txt);
+        left_section.add(pub_layout);
+        right_section.add(btn);
+        main_layout.add(left_section);
+        main_layout.add(right_section);
+        main_layout.display();
+    }
 
-        ~ZeusPublisher(){
-            pub.shutdown();
-        }
+    ~ZeusPublisher() { pub.shutdown(); }
 
-        void publish_msg() {
-            value_pub.data = pub_value_txt.value;
-            pub.publish(value_pub);
-        }
+private:
+    void publish_msg() {
+        value_pub.data = pub_value_txt.value;
+        pub.publish(value_pub);
+    }
 
-    void start_thread () {
+    void start_thread() {
         thr_running = true;
         pub_value_txt.disabled = true;
         btn.description = "Stop Publishing";
         thr = std::thread(&ZeusPublisher::thread_loop, this);
     }
 
-    void thread_loop () {
+    void thread_loop() {
         while (thr_running) {
             value_pub.data = pub_value_txt.value;
             pub.publish(value_pub);
         }
     }
 
-    void stop_thread () {
+    void stop_thread() {
         thr_running = false;
         pub_value_txt.disabled = false;
         btn.description = "Start Publishing";
@@ -103,5 +99,4 @@ class ZeusPublisher {
             thr.join();
         }
     }
-
 };
--- cpp/notebooks/jupyter_ros_utils/subscriber.hpp
+++ cpp/notebooks/jupyter_ros_utils/subscriber.hpp
@@ -1,80 +1,100 @@
-#include "xwidgets/xbutton.hpp"
-#include "xwidgets/xbox.hpp"
-#include "xwidgets/xtextarea.hpp"
-#include <boost/function.hpp>
-#include <sstream>
-#include <ros/ros.h>
-#include <ros/spinner.h>
+#pragma once
 
-#include <cstring>
-#include <iostream>
+#include <sstream>
 #include <string>
 
-void ros_init(){
-    int argc = 1;
-    char *argv[1];
-    argv[0] = new char[9];
-    strcpy(argv[0], "jupyter_ros_sub");
-    ros::init(argc, argv, "jupyter_ros_sub");
-    static ros::AsyncSpinner spinner(4);
-    spinner.start();
-}
+#include <boost/function.hpp>
+#include <ros/ros.h>
 
-template<class Arg>
+#include <xwidgets/xbutton.hpp>
+#include <xwidgets/xbox.hpp>
+#include <xwidgets/xtextarea.hpp>
+
+#include "init.hpp"
+
+/**
+ * @brief ROS Subscriber with widgets for jupyter notebook
+ * 
+ * @tparam Arg 
+ */
+template <class Arg>
 class ZeusSubscriber {
-    private:
-        xw::vbox box;
-        xw::button button;
-        ros::Subscriber sub;
-        xw::textarea textarea;
-        bool append = false;
+private:
+    ros::Subscriber sub;
+    int sub_queue_size = 10;
 
-        using CbType = void(Arg);
-        std::function<CbType> _cb;
+    xw::vbox box;
+    xw::button button;
+    xw::textarea textarea;
 
-    public:
-        ZeusSubscriber(std::string topic, std::function<CbType> cb_, bool append_=false): _cb(cb_), append(append_) {
-            ros::NodeHandle nh;
-            sub = nh.subscribe(topic, 10, &ZeusSubscriber::cb, this);
-            button.on_click([this]{toggle();});
-            button.description = "[ " + topic + " ]";  // actual start state might not be subscribed... sometimes
+    bool append = false;
 
-            box.add(button);
-            box.add(textarea);
-            box.display();  // @TODO: adjust width of the box
-        }
+    using CbType = void(Arg);
+    std::function<CbType> _cb;
 
-        ~ZeusSubscriber(){
+public:
+    ZeusSubscriber(std::string topic, std::function<CbType> cb_, bool append_ = false)
+            : _cb(cb_)
+            , append(append_) {
+        ros::NodeHandle nh;
+        sub = nh.subscribe(topic, sub_queue_size, &ZeusSubscriber::cb, this);
+        button.on_click([this] { toggle(); });
+        button.description = "[ " + topic + " ]";  // actual start state might not be subscribed... sometimes
+
+        box.add(button);
+        box.add(textarea);
+        box.display();  // @TODO: adjust width of the box
+    }
+
+    ~ZeusSubscriber() { sub.shutdown(); }
+
+    ZeusSubscriber(std::string topic, CbType* cb_, bool append_ = false)
+            : ZeusSubscriber(topic, std::function<CbType>(cb_), append_) {}
+
+    ZeusSubscriber(std::string topic, boost::function<CbType> cb_, bool append_ = false)
+            : ZeusSubscriber(topic, std::function<CbType>(cb_), append_) {}
+
+    /**
+     * @brief Get the ROS Subscriber Queue Size
+     */
+    inline int getSubscriberQueueSize() { return sub_queue_size; }
+
+    /**
+     * @brief Set the ROS Subscriber Queue Size
+     */
+    inline void setSubscriberQueueSize(int val) { sub_queue_size = val; }
+
+private:
+    /**
+     * @brief The callback for ros subscriber
+     * The std::cout is redirected to stringstream so we can add it to textarea
+     */
+    void cb(const Arg& arg) {
+        std::stringstream zeus_cout;                  // do smthg for color
+        std::streambuf* coutbuf = std::cout.rdbuf();  // save old buf
+        // divert cout to string stream
+        std::cout.rdbuf(zeus_cout.rdbuf());
+
+        _cb(arg);
+        // set the text output as the captured stdout
+        textarea.value = (append ? textarea.value() : "") + zeus_cout.str();
+
+        // restore cout
+        std::cout.rdbuf(coutbuf);
+    }
+
+    /**
+     * @brief Start & stop subscribing when button is clicked
+     */
+    void toggle() {
+        auto topic = sub.getTopic();
+        if (sub) {
             sub.shutdown();
+            button.description = "Start [ " + topic + " ]";
+        } else {
+            ros::NodeHandle nh;
+            sub = nh.subscribe(topic, sub_queue_size, &ZeusSubscriber::cb, this);
+            button.description = "Stop [ " + topic + " ]";
         }
-
-        ZeusSubscriber(std::string topic, CbType* cb_, bool append_=false): ZeusSubscriber(topic, std::function<CbType>(cb_), append_) {}
-        ZeusSubscriber(std::string topic, boost::function<CbType> cb_, bool append_=false): ZeusSubscriber(topic, std::function<CbType>(cb_), append_) {}
-
-        void cb(const Arg& arg) {
-            std::stringstream zeus_cout;  // do smthg for color
-            std::streambuf *coutbuf = std::cout.rdbuf(); //save old buf
-            // divert cout to string stream
-            std::cout.rdbuf(zeus_cout.rdbuf());
-
-            _cb(arg);
-            // set the text output as the captured stdout
-            textarea.value = (append ? textarea.value() : "") + zeus_cout.str();
-
-            // restore cout
-            std::cout.rdbuf(coutbuf);
-
-            }
-
-        void toggle() {
-            auto topic = sub.getTopic();
-            if (sub) {
-                sub.shutdown();
-                button.description = "Start [ " + topic + " ]";
-            } else {
-                ros::NodeHandle nh;
-                sub = nh.subscribe(topic, 10, &ZeusSubscriber::cb, this);
-                button.description = "Stop [ " + topic + " ]";
-            }
-        }
+    }
 };
